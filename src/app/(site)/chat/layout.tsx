03e38caae22842a2d1c80f0bbbb90faa425cff43
Harsh Singh <harsh.singh30112004@gmail.com>
2025-03-31 12:33:54 +0530

    integrating conversation List

--- src/app/(site)/chat/layout.tsx
+++ src/app/(site)/chat/layout.tsx
@@ -1,15 +1,30 @@
+import { getConversations } from '@/features/chats/actions/getConversations'
+import ConversationList from '@/features/chats/components/conversation-list'
+import Loading from '@/features/chats/components/Loading'
+import { PartialConversation } from '@/features/chats/schema'
 import Sidebar from '@/features/general/components/Sidebar'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 type Props = {
   children:React.ReactNode
 }
 
-function layout({children}: Props) {
+
+
+async function layout({children}: Props) {
+  const conversations:PartialConversation[] = await getConversations();
+  console.log(conversations);
   return (
     <div>
       <Sidebar>
-        {children}
+        
+        <div className='h-full'>
+          <ConversationList initialItems = {conversations} />
+          <Suspense fallback={<Loading/>}>
+            {children}
+          </Suspense>
+
+        </div>
       </Sidebar>
     </div>
   )
