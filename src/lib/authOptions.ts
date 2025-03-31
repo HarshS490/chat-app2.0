f26ec07cc8cbb706db9581e3240a125ac5f461fe
Harsh Singh <harsh.singh30112004@gmail.com>
2025-03-31 12:31:46 +0530

    augmenting next-auth types

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -1,4 +1,4 @@
-import NextAuth, { Account, NextAuthConfig } from "next-auth";
+import NextAuth, { Account,  NextAuthConfig, User } from "next-auth";
 import Google from "next-auth/providers/google";
 import { LOGIN_URL } from "./apiEndPoints";
 import axios from "axios";
@@ -10,7 +10,7 @@ export interface CustomUser {
   image?: string | null;
   token?: string | null;
   provider?: string | null;
-}
+};
 
 const authOptions: NextAuthConfig = {
   providers: [
@@ -29,9 +29,13 @@ const authOptions: NextAuthConfig = {
       user,
       account,
     }: {
-      user: CustomUser;
+      user: User
       account: Account | null;
     }) {
+      /**
+       * This method willl be called only once i.e during SigIn
+       * Returns a truthy value telling whether user is authorized or not.
+       */
       try {
         const payload = {
           email: user.email,
@@ -40,31 +44,41 @@ const authOptions: NextAuthConfig = {
           image: user?.image,
           provider: account?.provider,
         };
+        console.log("SignIN payload: ",payload);
         const { data } = await axios.post(LOGIN_URL, payload);
+        
         user.id = data?.user?.id.toString();
         user.token = data?.user?.token;
         user.provider = data?.user?.provider;
 
+        // console.log("Sign In User: ",user);
         return true;
       } catch (error) {
         console.log(error);
         return false;
       }
     },
-    async session({ session, token }) {
-      console.log("session_token", token);
-      if (token.userId) {
-        session.user.id = token.userId as string;
-      }
-      console.log("sessin:", session);
-      return session;
-    },
     async jwt({ token, user }) {
+      /**
+       *
+       * user is avaialable only during signIn.
+       */
       if (user) {
         token.userId = user.id;
+        token.backendToken = user.token;
       }
-      console.log("token: ", token);
+      // console.log("JWT TOKEN : ",token);
+      // console.log("JWT USER :",user );
       return token;
+    },
+    async session({ session, token }) {
+      if (token.userId) {
+        session.user.id = token.userId as string;
+        session.backendToken = token.backendToken;
+      }
+      // console.log("Session:", session);
+      // console.log("Session_token : ",token);
+      return session;
     },
   },
   pages: {
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,27 @@
+import { DefaultSession } from "next-auth";
+import "next-auth/jwt";
+
+declare module "next-auth"{
+  interface Session{
+    user:{
+      id:string;
+    }& DefaultSession["user"]
+    backendToken:string;
+  }
+  interface User {
+    id:string;
+    name:string;
+    email:string;
+    image:string;
+    token:string;
+    provider:string;
+  }
+};
+
+declare module "next-auth/jwt"{
+  interface JWT{
+    userId:string|undefined;
+    backendToken:string;
+  }
+}
+
