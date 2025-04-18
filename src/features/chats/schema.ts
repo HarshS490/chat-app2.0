d6db618465fd39f388def19911551acf7336a63c
Harsh Singh <harsh.singh30112004@gmail.com>
2025-04-18 20:06:45 +0530

    creating baseuser and messagetype schemas

--- src/features/chats/schema.ts
+++ src/features/chats/schema.ts
@@ -1,4 +1,6 @@
-export type PartialConversation = {
+import { BaseUser } from "../users/schema";
+
+export type CompleteConversation = {
   userId: string;
   conversationId: string;
   conversation: {
@@ -34,3 +36,73 @@ export type PartialConversation = {
     }>;
   };
 };
+
+export type PartialConversation = {
+  id: string;
+  name: string | null;
+  image: string | null;
+  isGroup: boolean;
+  createdAt: Date;
+  lastMessageAt: Date;
+  users: Array<{
+    user: {
+      id: string;
+      email: string;
+      name: string;
+      image: string;
+    };
+    userId: string;
+    conversationId: string;
+  }>;
+};
+
+export type Conversation = {
+  isGroup: boolean | null;
+  name: string | null;
+  id: string;
+  image: string | null;
+  createdAt: Date;
+  lastMessageAt: Date;
+};
+
+export type messages = {
+  messages: Array<{
+    id: string;
+    image: string | null;
+    createdAt: Date;
+    updatedAt: Date;
+    userId: string;
+    conversationId: string;
+    body: string | null;
+    public_id: string | null;
+    isEdited: boolean | null;
+    createdBy: {
+      name: string | null;
+    };
+  }>;
+};
+
+export type BaseMessage = {
+  id: string;
+  image: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+  userId: string;
+  conversationId: string;
+  body: string | null;
+  public_id: string | null;
+  isEdited: boolean | null;
+};
+
+export type FullMessageType = {createdBy:BaseUser} & BaseMessage;
+
+
+export type SocketMessageType = {
+  body:string;
+  image:string|null;
+  chatId: string;
+  createdBy: string;
+  name: string;
+  createdAt : Date;
+  timeStamp?: number;
+}
--- src/features/users/schema.ts
+++ src/features/users/schema.ts
@@ -1,13 +1,35 @@
-export type PartialUser={
+export type PartialUser = {
   name: string | null;
   id: string;
   image: string | null;
   createdAt: Date;
 };
 
-export type SessionUser ={
-  name: string,
-  email: string,
-  image: string,
-  id: string
-}
+export type SessionUser = {
+  name: string;
+  email: string;
+  image: string;
+  id: string;
+};
+
+export type BaseUser = {
+  name: string | null;
+  id: string;
+  email: string;
+  image: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type BaseFriendRequest = {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+  senderId: string;
+  receiverId: string;
+  status: "accepted" | "declined" | "pending";
+};
+
+export type FullFriendRequest = { sender: BaseUser } & {
+  receiver: BaseUser;
+} & BaseFriendRequest;
