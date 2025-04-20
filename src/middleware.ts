84652cdebe65b8910e15097808af695825ea50ff
Harsh Singh <harsh.singh30112004@gmail.com>
2025-04-21 01:01:59 +0530

    implementing middleware for route protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,24 @@
-export {auth as middleware } from "@/lib/authOptions";
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export const config={
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|conversations).*)"] 
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+
+  const isAuth = !!token;
+  const isAuthPage = req.nextUrl.pathname.startsWith("/auth");
+
+  if (!isAuth && !isAuthPage) {
+    return NextResponse.redirect(new URL("/auth", req.url));
+  }
+
+  if (isAuth && isAuthPage) {
+    return NextResponse.redirect(new URL("/chat", req.url));
+  }
+
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/chat/:path*", "/users/:path*", "/auth"],
+};
